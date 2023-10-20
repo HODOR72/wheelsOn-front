3bc750e49da30fab64f00f2db603028e78238414
notlikethiss <notlikeethiss@gmail.com>
2023-10-20 18:51:35 +0300

    added calculate page

--- widgets/base-layout.tsx
+++ widgets/base-layout.tsx
@@ -1,13 +1,18 @@
-import React, { FunctionComponent } from "react"
+import type { CSSProperties } from "react";
+import React, { FunctionComponent } from "react";
 
 interface BaseLayout {
-	children: JSX.Element[] | JSX.Element
+	children: JSX.Element[] | JSX.Element;
+	style?: CSSProperties;
 }
 
-export const BaseLayout: FunctionComponent<BaseLayout> = ({ children }) => {
+export const BaseLayout: FunctionComponent<BaseLayout> = ({
+	children,
+	style = { height: "100vh" },
+}) => {
 	return (
-		<div className="container pt-1 pb-3" style={{ height: "100vh" }}>
+		<div className="container pt-1 pb-3" style={style}>
 			{children}
 		</div>
-	)
-}
+	);
+};
